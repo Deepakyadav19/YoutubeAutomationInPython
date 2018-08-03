2c9db40160f2298c09d3164cd3a1cb170d8ab3c3
Sudhanshu Mishra <sudhanshu.mishra.8826@gmail.com>
2018-08-03 20:28:16 +0530

    Add files via upload

---
+++ Youtube.py
@@ -0,0 +1,87 @@
+from selenium import webdriver
+from time import sleep
+import re
+import pickle
+import sys
+from bs4 import BeautifulSoup
+
+try:
+    topic=input("Enter the search keyword for the video : ")
+except Exception as e:
+    print("Invalid input ")
+    ch=input("Do You Want to See the Error ????? (y/n) ")
+    if ch=='y':
+        print(e)
+    sys.exit()
+try:
+    driver = webdriver.Chrome()
+except Exception as e:
+    print("ERROR : DRIVER NOT FOUND")
+    ch=input("Do You Want to See the Error ????? (y/n) ")
+    if ch=='y':
+        print(e)
+    sys.exit()
+try:
+    driver.get('https://www.youtube.com/')
+except Exception as e:
+    print("ERROR : CONNECTION PROBLEM") 
+    driver.quit()
+    ch=input("Do You Want to See the Error ????? (y/n) ")
+    if ch=='y':
+        print(e)
+    sys.exit()
+try:
+    search_box=driver.find_element_by_xpath('//*[@id="search"]')
+    search_box.send_keys(topic)
+except Exception as e:
+    print("!!!!!!!Error !!!!!!")
+    ch=input("Do You Want to See the Error ????? (y/n) ")
+    if ch=='y':
+        print(e)
+    driver.quit()
+    sys.exit()
+try:
+    driver.find_element_by_xpath('//*[@id="search-icon-legacy"]').click()
+except Exception as e:
+    print('!!!!!!! ERROR !!!!!!!')
+    ch=input("Do You Want to See the Error ????? (y/n) ")
+    if ch=='y':
+        print(e)
+    driver.quit()
+    sys.exit()
+sleep(5)
+Youtube_source = driver.page_source
+soup = BeautifulSoup(Youtube_source, 'lxml')
+for div in soup.find_all('a', {'id': 'video-title'}):
+    url='https://www.ssyoutube.com'+div.get('href')
+    break
+try:
+    driver.get(url)
+except Exception as e:
+    print("ERROR : CONNECTION PROBLEM") 
+    driver.quit()
+    ch=input("Do You Want to See the Error ????? (y/n) ")
+    if ch=='y':
+        print(e)
+    sys.exit()
+downloadPage=driver.page_source
+soup2=BeautifulSoup(downloadPage,'lxml')
+sleep(5)
+try:
+    driver.find_element_by_class_name('def-btn-box').click()
+except Exception as e:
+    print('!!!!!!!! ERROR !!!!!!!')
+    ch=input("Do You Want to See the Error ????? (y/n) ")
+    if ch=='y':
+        print(e)
+    driver.quit()
+    sys.exit()
+ex=input('Enter Anything to Exit')
+driver.quit()
+
+
+
+    
+    
+    
+
